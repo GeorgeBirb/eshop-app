77d5ca43f605e24dc0f8ca53f5f0c6ee7a2c885c
George Birb <53330328+GeorgeBirb@users.noreply.github.com>
2023-01-07 00:13:27 +0200

    eShop Backend Initial Commit
    eShop Backend Initial Commit

---
+++ EshopAPI/Models/ShopCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace EshopAPI.Models
+{
+    public class ShopCategory
+    {
+        public int ShopCategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
