370e619b4d980895d218c441e17788a0e5488631
George Birb <53330328+GeorgeBirb@users.noreply.github.com>
2023-01-09 00:37:07 +0200

    Stable Backend
    Stable Backend

---
+++ EshopAPI/EshopAPI/Models/Shops.cs
@@ -0,0 +1,14 @@
+﻿namespace EshopAPI.Models
+{
+    public class Shops
+    {
+        public int ShopID { get; set; }
+
+        public string ShopName { get; set; }
+
+        public int ShopCategoryID { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
