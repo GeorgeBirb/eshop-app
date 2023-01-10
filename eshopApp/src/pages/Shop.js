68f606b32f74a1104ef9f366dd5fd496dc975afe
George Birb <53330328+GeorgeBirb@users.noreply.github.com>
2023-01-11 01:17:54 +0200

    CRUD Stabled with Entity Framework 7.01
    CRUD Stabled with Entity Framework 7.01

---
+++ eshopApp/src/pages/Shop.js
@@ -0,0 +1,14 @@
+import ShopDataDisplay from '../components/ShopTable'
+import React from 'react';
+
+
+function Shop() {
+    return (
+        <div className="container">
+            <h2>Our Shops!!!</h2>
+            <span><br /><br /></span>
+            <ShopDataDisplay />
+        </div>
+    );
+}
+export default Shop;
