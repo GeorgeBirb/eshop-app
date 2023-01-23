6ffa8362db02fd362e819c5c09b397d88ebbe5da
George Birb <53330328+GeorgeBirb@users.noreply.github.com>
2023-01-24 00:41:23 +0200

    Add files via upload

---
+++ EshopAPI/Controllers/ShopCategoryController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EshopAPI.Models;
+using EshopAPI.Data;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace EshopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopCategoryController : Controller
+    {
+        private readonly EshopDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public ShopCategoryController(EshopDbContext context, IConfiguration configuration) 
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            List<ShopCategory> shopCategories = _context.ShopCategories.ToList();
+            string json = JsonConvert.SerializeObject(shopCategories);
+            return json;
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetById(int id)
+        {
+            ShopCategory shopCategory = _context.ShopCategories.Single(a => a.ShopCategoryId == id);
+            return new JsonResult(shopCategory);
+        }
+
+        public ShopCategory GetByShopCategoryId(int id)
+        {
+            ShopCategory shopCategory = _context.ShopCategories.Single(a => a.ShopCategoryId == id);
+            return shopCategory;
+        }
+
+        [HttpPost]
+        public JsonResult Post(ShopCategory shopCategory)
+        {
+            _context.Attach(shopCategory);
+            _context.Entry(shopCategory).State = EntityState.Added;
+            _context.SaveChanges();
+            return new JsonResult("Inserted Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(ShopCategory shopCategory)
+        {
+            _context.Attach(shopCategory);
+            _context.Entry(shopCategory).State = EntityState.Modified;
+            _context.SaveChanges();
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            ShopCategory shopCategory = _context.ShopCategories.Single(a => a.ShopCategoryId == id);
+            _context.Attach(shopCategory);
+            _context.Entry(shopCategory).State = EntityState.Deleted;
+            _context.SaveChanges();
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
---
+++ EshopAPI/Data/EshopDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using EshopAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EshopAPI.Data;
+
+public partial class EshopDbContext : DbContext
+{
+    public EshopDbContext()
+    {
+    }
+
+    public EshopDbContext(DbContextOptions<EshopDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Shop> Shops { get; set; }
+
+    public virtual DbSet<ShopCategory> ShopCategories { get; set; }
+    public object Shop { get; internal set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=EShopDB; Integrated Security=true; Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Shop>(entity =>
+        {
+            entity.HasKey(e => e.ShopId).HasName("PK__Shops__67C5562978499A44");
+
+            entity.HasOne(d => d.ShopCategory).WithMany(p => p.Shops)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ShopCategory");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    internal void Remove(object value)
+    {
+        throw new NotImplementedException();
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
